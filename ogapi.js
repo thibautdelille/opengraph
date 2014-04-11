851fa0738fd88279b6fc78a240f223db3519b1dd
Thibaut Delille <thibaut@jvst.us>
2014-04-11 16:17:12 -0700

    add vows

---
+++ ogapi.js
@@ -0,0 +1,10 @@
+/* GET users listing. */
+exports = ogapi;
+
+var ogapi = function(){
+	return this;
+};
+
+ogapi.prototype.getData = function(url){
+
+};
--- ogapi/index.js
+++ ogapi/index.js
@@ -1,9 +1,6 @@
-var Controller = require('../controllers');
 
-/* GET home page. */
-exports.index = function(req, res){
-  res.render('index', { title: 'OpenGraph API', api: '' });
-};
-exports.sumbit = function(req, res){
-  res.render('index', { title: 'OpenGraph API', api: req.body.url });
+module.exports = ogapi;
+
+var ogapi = function(){
+
 };
